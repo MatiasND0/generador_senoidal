7e20755b045d2da1fd987c0ae932c2299bc3adbb
Matias Diaz <Matias>
2023-06-27 14:02:44 -0300

    Encoder variables rename

--- Generador/Generador.srcs/sources_1/new/tuning.v
+++ Generador/Generador.srcs/sources_1/new/tuning.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
 module tuning(
-    input clk,
-    input reset,
+    input CLK,
+    input RST,
     input A,
     input B,
     output reg signed [23:0] count
@@ -10,48 +10,48 @@ module tuning(
 
 (*ASYNC_REG="TRUE"*)reg[1:0] sync, AB; // synchronization registers
 reg[1:0] state;
-localparam S00=2'b00, S01=2'b01, S10=2'b10, S11=2'b11; // states
+localparam S0=2'b00, S1=2'b01, S2=2'b10, S3=2'b11; // states
 
-always @ (posedge clk) // two-stage input synchronizer
+always @ (posedge CLK) // two-stage input synchronizer
     begin
         sync <= {A,B};
         AB <= sync;
     end
 
-always @(posedge clk) // always block to compute output
+always @(posedge CLK) // always block to compute output
     begin 
-        if(reset) begin
-            state <= S00;
+        if(RST) begin
+            state <= S0;
             count <= 23'b0;
         end else
             case(state)              
-                S00: if(AB == 2'b01) begin
+                S0: if(AB == 2'b01) begin
                         count <= count-23'b1;
-                        state <= S01;
+                        state <= S1;
                     end else if(AB == 2'b10) begin
                         count <= count+23'b1;
-                        state <= S10;
+                        state <= S2;
                     end                                        
-                S01: if(AB == 2'b00) begin
+                S1: if(AB == 2'b00) begin
                         count <= count+23'b1;
-                        state <= S00;
+                        state <= S0;
                     end else if(AB == 2'b11) begin
                         count <= count-23'b1;
-                        state <= S11;
+                        state <= S3;
                     end                      
-                S10: if(AB == 2'b00) begin
+                S2: if(AB == 2'b00) begin
                         count <= count-23'b1;
-                        state <= S00;
+                        state <= S0;
                     end else if(AB == 2'b11) begin
                         count <= count+23'b1;
-                        state <= S11;
+                        state <= S3;
                     end                     
-                S11: if(AB == 2'b01) begin
+                S3: if(AB == 2'b01) begin
                         count <= count+23'b1;
-                        state <= S01;
+                        state <= S1;
                     end else if(AB == 2'b10) begin
                         count <= count-23'b1;
-                        state <= S10;
+                        state <= S2;
                     end
             endcase
     end 
